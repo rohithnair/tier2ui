f4d232beb360f09e2f5d4773c45797d3949c0dc9
rohithnair <rrohithr@gmail.com>
2019-04-22 18:43:20 +0100

    data table

--- tier2appui/src/App.js
+++ tier2appui/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 
 class App extends Component {
   render() {
--- tier2appui/src/Header.js
+++ tier2appui/src/Header.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
