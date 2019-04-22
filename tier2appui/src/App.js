6614c944fc6634457f7acaae92d768ab86271996
rohithnair <rrohithr@gmail.com>
2019-04-22 23:32:48 +0100

    axios

--- tier2appui/src/App.js
+++ tier2appui/src/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import MUIDataTable from "mui-datatables";
+const columns = ["Name", "Company", "City", "State"];
+
+const data = [
+ ["Joe James", "Test Corp", "Yonkers", "NY"],
+ ["John Walsh", "Test Corp", "Hartford", "CT"],
+ ["Bob Herm", "Test Corp", "Tampa", "FL"],
+ ["James Houston", "Test Corp", "Dallas", "TX"],
+];
 
 class App extends Component {
+
+
   render() {
     return (
       <div className="App">
-        
+        <MUIDataTable
+  data={data}
+  columns={columns}
+/>
       </div>
     );
   }
