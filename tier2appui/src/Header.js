228b36d529335df08f0e1c7b469e1096d8465ee0
rohithnair <rrohithr@gmail.com>
2020-05-18 00:36:56 +0100

    csv download

--- tier2appui/src/Header.js
+++ tier2appui/src/Header.js
@@ -5,7 +5,8 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { withRouter } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
- 
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 const styles = {
  
   menuButton: {
@@ -28,6 +29,14 @@ const styles = {
 
 function Header(props) {
   const { classes ,history} = props;
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <div className={classes.root}>
       <AppBar position="static">
@@ -54,6 +63,23 @@ function Header(props) {
           <Button className={classes.Border} color="inherit" onClick={() => history.push('/deleted')}>Deleted companies</Button>
        : <Button className={classes.Button} color="inherit" onClick={() => history.push('/deleted')}>Deleted companies</Button>
             } 
+
+<Button className={classes.Button} color="inherit" aria-haspopup="true" onClick={handleClick}>
+ Csv Download
+</Button>
+<Menu
+  anchorEl={anchorEl}
+  keepMounted
+  open={Boolean(anchorEl)}
+  onClose={handleClose}
+>
+  <MenuItem onClick={handleClose}  component="a" href="https://uktiersponsors.blob.core.windows.net/excel/UkTiersponsors_All.csv"  rel="nofollow noopener noreferrer"
+                >All</MenuItem>
+    <MenuItem onClick={handleClose}  component="a" href="https://uktiersponsors.blob.core.windows.net/excel/UkTiersponsors_Recent.csv"   rel="nofollow noopener noreferrer"
+                >Recent</MenuItem>
+   <MenuItem onClick={handleClose}  component="a" href="https://uktiersponsors.blob.core.windows.net/excel/UkTiersponsors_Deleted.csv"  rel="nofollow noopener noreferrer"
+                >Deleted</MenuItem>
+</Menu>
 
 <Button className={classes.Button} color="inherit" href="https://github.com/rohithnair/tier2ui" target="_blank" rel="nofollow noopener noreferrer">Source Code</Button>
           
